eb6a8094aff8d502d3e82d8d38798fcbf336ba59
Mindia Arabuli <mindiaarabuli4@gmail.com>
2026-06-16 15:29:27 +0400

    core/init

---
+++ features/landing/components/Landing.tsx
@@ -0,0 +1,13 @@
+import AboutUs from "./AboutUs";
+import Statistic from "./Statistic";
+
+const Landing = () => {
+  return (
+    <div>
+      <AboutUs />
+      <Statistic />
+    </div>
+  );
+};
+
+export default Landing;
